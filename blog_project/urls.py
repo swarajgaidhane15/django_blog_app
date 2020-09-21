e4439a8a6b7d9ff1ea5faf7d3f4c2ab3f83ea665
swarajgaidhane15 <rutvikgaidhane1508@gmail.com>
2020-09-21 12:30:05 +0530

    Forgot password updated

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -26,9 +26,12 @@ urlpatterns = [
          auth_views.PasswordResetConfirmView.as_view(
              template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-
